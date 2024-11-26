59940a5c2dc1c5d168ed76107fd334f0fd101f57
ignaceM <ignacemasirika00@gmail.com>
2024-11-26 08:09:48 +0200

    v

--- fichiers JS/share.js
+++ fichiers JS/share.js
@@ -1,19 +1,21 @@
 
-const share = document.getElementById("share")
+const shareButtons = document.querySelectorAll(".share");
 
-share.addEventListener('click', async () => {
-    if (navigator.share) {
-        try {
-            await navigator.share({
-                title: 'ignace App ! Meilleure application de développement personnel',
-                text: 'Découvrez ce site incroyable !',
-                url: window.location.href 
-            });
-            console.log('Contenu partagé avec succès');
-        } catch (error) {
-            console.error('Erreur lors du partage :', error);
+shareButtons.forEach(shareButton => {
+    shareButton.addEventListener('click', async () => {
+        if (navigator.share) {
+            try {
+                await navigator.share({
+                    title: 'Ignace App ! Meilleure application de développement personnel',
+                    text: 'Découvrez ce site incroyable !',
+                    url: window.location.href
+                });
+                console.log('Contenu partagé avec succès');
+            } catch (error) {
+                console.error('Erreur lors du partage :', error);
+            }
+        } else {
+            alert('Le partage n’est pas pris en charge sur ce navigateur.');
         }
-    } else {
-        alert('Le partage n’est pas pris en charge sur ce navigateur.');
-    }
+    });
 });
